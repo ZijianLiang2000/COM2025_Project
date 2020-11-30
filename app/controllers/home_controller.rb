b69742aa6e89da500974f1654e0ec2edcb10ab70
ZijianLiang2000 <brianbrain2010@hotmail.com>
2020-11-30 20:35:38 +0000

    Added Search Team by Nickname API function

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,47 @@
 class HomeController < ApplicationController
   def home
   end
+
+  # find_team method will search NBA Team by api, with a nickname of team, eg:'Hornets'
+  def find_team(nickname)
+    request_api(
+      "https://api-nba-v1.p.rapidapi.com/teams/nickName/#{nickname}"
+    )
+  end
+
+  def search
+    # Teams will return requested api of "https://api-nba-v1.p.rapidapi.com/teams/nickName/Hornets"
+    teams = find_team(params[:team])
+
+    unless teams
+      flash[:alert] = 'Team not found'
+      return render action: :home
+    end
+    
+    # @team parameter will be first item under teams api
+    @team = teams["api"]["teams"].first
+    @player = find_player(@team['teamId'])
+  end
+
+  def find_player(team_id)
+    # query = URI.encode("#{team_id}")
+    request_api("https://api-nba-v1.p.rapidapi.com/players/teamId/#{team_id}")
+    
+  end
+  
+  def request_api(url)
+    response = Excon.get(
+      url,
+      headers: {
+        'X-RapidAPI-Host' => URI.parse(url).host,
+        # 'X-RapidAPI-Key' => ENV.fetch('RAPIDAPI_API_KEY')
+        'X-RapidAPI-Key' => '81f17623camshd2b99a09f5ec7b9p1cf6aejsn282365a0a05e'
+        # KEY is: 81f17623camshd2b99a09f5ec7b9p1cf6aejsn282365a0a05e
+      }
+    )
+
+    return nil if response.status != 200
+
+    JSON.parse(response.body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   get 'navigation_contact', to:"contact#contact"
 
-
+  get '/search' => 'home#search'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
