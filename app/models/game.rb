0877fafb7f58482e5c815ad9f68c300aac0f14d7
ZijianLiang2000 <brianbrain2010@hotmail.com>
2020-12-13 17:31:12 +0000

    Added some comments in model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,18 +1,28 @@
 class Game < ApplicationRecord
-    # presence would validate whether attribute exists
-    #uniqueness would validate whether attribute isn't redundant
-    # deleted presence and uniqueness function for 'id'
+
+    # Game model
+    # |_ id (Generated and Validates defaultly)
+    # |_ homeTeamName (Must exist, Validates names)
+    # |_ awayTeamName (Must exist, Validates names)
+    # |_ homeTeamScore (Can be nil, due to a game can be scheduled, but not yet played)
+    # |_ awayTeamScore (Can be nil, due to a game can be scheduled, but not yet played)
+    # |_ gameDate (Must exist)
+
+    # Game model has and belongs to many teams, which is a Many-to-Many association.
+    # Due to a Game can have many teams (which can be redirected to a home team and an away team)
+    # and a Team can have many games
     has_and_belongs_to_many :teams
     
-    # All NBA Team names to validate home team name and away team name. 
+    # All NBA Team names to validate home team name and away team name to be real NBA team names. 
     nbaTeams = ["Atlanta Hawks","Boston Celtics","Brooklyn Nets","Charlotte Hornets",
         "Chicago Bulls","Cleveland Cavaliers","Dallas Mavericks","Denver Nuggets","Detroit Pistons","Golden State Warriors","Houston Rockets",
         "Indiana Pacers","Los Angeles Clippers","Los Angeles Lakers","Memphis Grizzlies","Miami Heat","Milwaukee Bucks","Minnesota Timberwolves",
-        "New Orleans Pelicans","New York Knicks","Oklahoma City Thunders","Orlando Magic","Philadelphia 76ers","Phoenix Suns","Portland Trail Blazers",
+        "New Orleans Pelicans","New York Knicks","Oklahoma City Thunder","Orlando Magic","Philadelphia 76ers","Phoenix Suns","Portland Trail Blazers",
         "Sacramento Kings","San Antonio Spurs","Toronto Raptors","Utah Jazz","Washington Wizards"]
 
     validates :homeTeamName, presence: true, inclusion: { in: nbaTeams, message: "%{value} is not a valid team, please re-enter a NBA team" }
     validates :awayTeamName, presence: true, inclusion: { in: nbaTeams, message: "%{value} is not a valid team, please re-enter a NBA team" }
+    # Game model validates gameDate value, because it cannot be nil, but different teams may have games on same date
     validates :gameDate, presence: true
 
 end
