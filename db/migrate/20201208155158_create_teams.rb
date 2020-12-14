fdb6ccb6f0db7e9c687cb6e91535a5480956d6f5
ZijianLiang2000 <brianbrain2010@hotmail.com>
2020-12-14 15:51:47 +0000

    Push

--- db/migrate/20201208155158_create_teams.rb
+++ db/migrate/20201208155158_create_teams.rb
@@ -1,10 +1,8 @@
 class CreateTeams < ActiveRecord::Migration[5.1]
   def change
-    create_table :teams, do |t|
-      t.primary_key :id
+    create_table :teams do |t|
       t.string :teamName, null: false
       t.integer :gamesPlayedId
-
       t.timestamps
     end
   end
