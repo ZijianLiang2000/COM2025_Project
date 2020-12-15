53e422b7726fb6c3bd2f5e1807d5740d11caf6a5
ZijianLiang2000 <brianbrain2010@hotmail.com>
2020-12-15 10:53:11 +0000

    Added migration to create table if no existence

--- db/migrate/20201212001327_add_seasonal_teams.rb
+++ db/migrate/20201212001327_add_seasonal_teams.rb
@@ -1,6 +1,23 @@
 class AddSeasonalTeams < ActiveRecord::Migration[5.1]
   def change
     #To avoid contradiction, drop Users_teams association table if created 
+
+    drop_table :users, if_exists: true
+  #  Create devise users table
+    create_table :users do |t|
+      t.string "email", default: "", null: false
+      t.string "encrypted_password", default: "", null: false
+      t.string "reset_password_token"
+      t.datetime "reset_password_sent_at"
+      t.datetime "remember_created_at"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["email"], name: "index_users_on_email", unique: true
+      t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+      t.timestamps
+    end
+
     drop_table :users_teams, if_exists: true
     # Create  Users_teams association table
     create_join_table :users, :teams do |t|
